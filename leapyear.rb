f379729c31611147c789d5249ed5bb1df8b0e5b3
dburkeggc <DBurkeGGC@gmail.com>
2016-02-08 21:05:11 +0000

    update

--- leapyear.rb
+++ leapyear.rb
@@ -1,5 +1,5 @@
 class LeapYear
   def is_leap_year?(year)
-    return (year >= 1582 && year % 4 == 0 ? ( year % 100 == 0 ? ( year % 400 == 0 ? true : false ) : true ) : year > 0 && year % 4 == 0 ? true : false)
+    return (year >= 1752 && year % 4 == 0 ? ( year % 100 == 0 ? ( year % 400 == 0 ? true : false ) : true ) : year > 0 && year % 4 == 0 ? true : false)
   end
 end
